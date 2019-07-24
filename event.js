f3c023d0c0c6eec0a3faf21fb8665de17f6473fa
dhjw <deanhjw@gmail.com>
2019-07-23 20:45:33 -0700

    Improved efficiency

--- event.js
+++ event.js
@@ -16,29 +16,34 @@ chrome.alarms.onAlarm.addListener(function(a){
 function update(){
 	if(debug) console.log('update()');
 	chrome.storage.sync.get(['data'],function(obj){
-		if(!obj.data.addr){ if(debug) console.log('no address set, skipping run'); return; }
+		if(!obj.data || !obj.data.addr){ if(debug) console.log('no address set, skipping run'); return; }
+		var lr=JSON.parse(localStorage.getItem('lastresponse'));
+		var nows=Math.floor(Date.now()/1000);
+		var skipPayout=lr && lr.addr==obj.data.addr && lr.lastPayout && (nows-lr.lastPayout.paidOn<86400)?1:0;
 		var x0=new XMLHttpRequest(); x0.timeout=15000; x0.open("GET","https://api.ethermine.org/miner/"+obj.data.addr+"/currentStats",true);
 		var x1=new XMLHttpRequest(); x1.timeout=15000; x1.open("GET","https://api.ethermine.org/miner/"+obj.data.addr+"/settings",true);
-		var x2=new XMLHttpRequest(); x2.timeout=15000; x2.open("GET","https://api.ethermine.org/miner/"+obj.data.addr+"/payouts",true);
-		var xs=[x0,x1,x2];
+		var xs=[x0,x1];
+		if(!skipPayout){
+			var x2=new XMLHttpRequest(); x2.timeout=15000; x2.open("GET","https://api.ethermine.org/miner/"+obj.data.addr+"/payouts",true);
+			xs.push(x2);
+		}
 		onRequestsComplete(xs, function(xr, xerr){
-			for(let i=0;i<xs.length;i++) if(xs[i].status!==200){
-				console.log('api error xs=',xs);
-				return;
-			}
+			for(let i=0;i<xs.length;i++) if(xs[i].status!==200) return console.log('api error xs=',xs);
 			try { var r0=JSON.parse(x0.responseText); } catch(e){}
 			try { var r1=JSON.parse(x1.responseText); } catch(e){}
-			try { var r2=JSON.parse(x2.responseText); } catch(e){}
+			if(!skipPayout) try { var r2=JSON.parse(x2.responseText); } catch(e){}
 			if(debug) console.log('r0=',r0,'r1=',r1,'r2=',r2);
-			if(!r0 || !r1) return;
+			if(!r0 || !r1 || (!skipPayout && !r2)) return console.log('api error xs=',xs);
 			r={data:{}}
 			for(var a in r0.data) r.data[a]=r0.data[a];
 			for(var a in r1.data) r.data[a]=r1.data[a];
-			r.lastPayout=r2.data[0];
+			if(!skipPayout) r.lastPayout=r2.data[0]; else r.lastPayout=lr.lastPayout;
 			if(!r.data.unpaid) r.data.unpaid=0;
 			if(debug) console.log('r=',r);
 			updateBadge(r);
 		});
-		x0.send(); x1.send(); x2.send();
+		x0.send();
+		x1.send();
+		if(!skipPayout) x2.send();
 	});
 }
--- popup.js
+++ popup.js
@@ -7,19 +7,16 @@ function dataDisplay(r){
 	var coinspd=BigNumber(r.data.coinsPerMin.toString()).times(1440).toFixed(5);
 	var coinspm=BigNumber(r.data.coinsPerMin.toString()).times(1440).times(30).toFixed(5);
 	var coinspy=BigNumber(r.data.coinsPerMin.toString()).times(1440).times(365).toFixed(5);
-	var curpd=BigNumber(r.data.usdPerMin.toString()).times(r.currency_rate).times(1440).toFixed(2);
-	var curpm=BigNumber(r.data.usdPerMin.toString()).times(r.currency_rate).times(1440).times(30).toFixed(2);
-	var curpy=BigNumber(r.data.usdPerMin.toString()).times(r.currency_rate).times(1440).times(365).toFixed(2);
+	var curpd=BigNumber(r.data.usdPerMin.toString()).times(r.rates[r.currency]).times(1440).toFixed(2);
+	var curpm=BigNumber(r.data.usdPerMin.toString()).times(r.rates[r.currency]).times(1440).times(30).toFixed(2);
+	var curpy=BigNumber(r.data.usdPerMin.toString()).times(r.rates[r.currency]).times(1440).times(365).toFixed(2);
 	var r=getETA(r);
 	var html='<div id="mid_wrap">Hashrate: '+hashrate+'<br>Balance: '+unpaid+' / '+minpayout+'<br>ETA: '+r.eta+(r.lt24h?'<span class="lt24h" title="Total estimated time to payment threshold less than 24h from last payment. ETA set to 24h from then.">?</span>':'')+'<br>';
 	html+='</div>';
-	var csel='<select style="border:0;background:#fff;font-size:16px;padding:0" id="c">';
-	var rkeys=Object.keys(r.rates);
-	for(let i=0;i<rkeys.length;i++){
-		csel+='<option value="'+rkeys[i]+'"'+(rkeys[i]==r.currency?' selected="selected"':'')+'>'+rkeys[i];
-	}
+	var csel='<select id="c">';
+	for(var c in r.rates) csel+='<option value="'+c+'"'+(c==r.currency?' selected="selected"':'')+'>'+c;
 	csel+='</select>';
-	html+='<table><tr><th></th><th>Day</th><th>Month</th><th>Year</th></tr><tr><td>ETH</td><td>'+coinspd+'</td><td>'+coinspm+'</td><td>'+coinspy+'</td></tr><tr><td style="padding-left:0; padding-right:0">'+csel+'</td><td>'+curpd+'</td><td>'+curpm+'</td><td>'+curpy+'</td></tr></table>';
+	html+='<table><tr><th></th><th>Day</th><th>Month</th><th>Year</th></tr><tr><td>ETH</td><td>'+coinspd+'</td><td>'+coinspm+'</td><td>'+coinspy+'</td></tr><tr><td style="padding:0">'+csel+'</td><td>'+curpd+'</td><td>'+curpm+'</td><td>'+curpy+'</td></tr></table>';
 	html+='<a target="_blank" href="https://ethermine.org/miners/'+addr+'">Dashboard</a> | <a target="_blank" href="https://ethermine.org/miners/'+addr+'/payouts">Payouts</a> | <a target="_blank" href="https://ethermine.org/miners/'+addr+'/settings?ip='+r.data.ip+'">Settings</a>';
 	document.getElementById('data_wrap').innerHTML=html;
 	document.getElementById('c').addEventListener('change',()=>{ document.getElementById('c').blur(); updateCurrency(r); });
@@ -31,9 +28,7 @@ function dataDisplay(r){
 	updateBadge(r);
 }
 function updateCurrency(r){
-	localStorage.setItem('currency',document.getElementById('c').value);
 	r.currency=document.getElementById('c').value;
-	r.currency_rate=r.rates[r.currency];
 	localStorage.setItem('lastresponse',JSON.stringify(r));
 	updateData();
 }
@@ -43,61 +38,83 @@ function updateData(nocache){
 	// dont spam the api, just load from storage if opened too quickly
 	var lu=localStorage.getItem('lastupdate');
 	if(!lu) lu=0;
-	var now=Date.now();
-	if(now-lu<32000 && !nocache){ // less than interval due to retrieval time
+	var lr=JSON.parse(localStorage.getItem('lastresponse'));
+	var now=Date.now(), nows=Math.floor(now/1000);
+	if(now-lu<27500 && !nocache){ // less than interval due to retrieval time
 		if(debug) console.log('loading cached data');
-		dataDisplay(JSON.parse(localStorage.getItem('lastresponse')));
-		return;
+		return dataDisplay(lr);
 	}
-	var currency=localStorage.getItem('currency');
-	if(!currency) currency='USD';
+	var skipPayout=lr && lr.addr==addr && lr.lastPayout && nows-lr.lastPayout.paidOn<86400?1:0;
+	var skipRates=lr && lr.rates && lr.ratesTime && nows-lr.ratesTime<21600?1:0;
+	if(debug) console.log('skipPayout=',skipPayout,'skipRates=',skipRates);
 	var x0=new XMLHttpRequest(); x0.timeout=15000; x0.open("GET","https://api.ethermine.org/miner/"+addr+"/currentStats",true);
 	var x1=new XMLHttpRequest(); x1.timeout=15000; x1.open("GET","https://api.ethermine.org/miner/"+addr+"/settings",true);
-	var x2=new XMLHttpRequest(); x2.timeout=15000; x2.open("GET","https://api.ethermine.org/miner/"+addr+"/payouts",true);
-	var x3=new XMLHttpRequest(); x3.timeout=15000; x3.open("GET","https://api.exchangeratesapi.io/latest",true);
 	var x4=new XMLHttpRequest(); x4.timeout=15000; x4.open("GET","https://ipinfo.io/json",true); // get ip to append to settings url
-	var xs=[x0,x1,x2,x3,x4];
+	var xs=[x0,x1,x4]
+	if(!skipPayout){
+		var x2=new XMLHttpRequest(); x2.timeout=15000; x2.open("GET","https://api.ethermine.org/miner/"+addr+"/payouts",true);
+		xs.push[x2];
+	}
+	if(!skipRates){
+		var x3=new XMLHttpRequest(); x3.timeout=15000; x3.open("GET","https://api.exchangeratesapi.io/latest",true);
+		xs.push[x3];
+	}
 	onRequestsComplete(xs, function(xr, xerr){
 		for(let i=0;i<xs.length;i++) if(xs[i].status!==200){
 			document.getElementById('data_wrap').innerHTML='<div id="error">Error getting data from one or more APIs.<br>This should be temporary.</div>';
-			console.log('api error xs=',xs);
-			return;
+			return console.log('api error xs=',xs);
 		}
 		try { var r0=JSON.parse(x0.responseText); } catch(e){}
 		try { var r1=JSON.parse(x1.responseText); } catch(e){}
-		try { var r2=JSON.parse(x2.responseText); } catch(e){}
-		try { var r3=JSON.parse(x3.responseText); } catch(e){}
+		if(!skipPayout) try { var r2=JSON.parse(x2.responseText); } catch(e){}
+		if(!skipRates) try { var r3=JSON.parse(x3.responseText); } catch(e){}
 		try { var r4=JSON.parse(x4.responseText); } catch(e){}
-		if(debug) console.log('r0=',r0,'r1=',r1,'r2=',r2,'r3=',r3);
-		if(!r0 || !r1 || !r2 || (!r3 && currency!='USD') || !r4) return;
+		if(debug) console.log('r0=',r0,'r1=',r1,'r2=',r2,'r3=',r3,'r4=',r4);
+		if(!r0 || !r1 || (!skipPayout && !r2) || (!skipRates && !r3) || !r4){
+			document.getElementById('data_wrap').innerHTML='<div id="error">Error getting data from one or more APIs.<br>This should be temporary.</div>';
+			return console.log('api error xs=',xs);
+		}
 		r={data:{}}
 		for(var a in r0.data) r.data[a]=r0.data[a];
 		for(var a in r1.data) r.data[a]=r1.data[a];
-		r.lastPayout=r2.data[0];
-		r.data.ip=r4.ip;
-		var tmp=[];
-		// convert EUR rates to USD rates
-		r3.rates.EUR=1;
-		var rkeys=Object.keys(r3.rates);
-		for(let i=0;i<rkeys.length;i++){
-			var inusd=new BigNumber(r3.rates[rkeys[i]]).div(r3.rates.USD);
-			tmp.push([rkeys[i],inusd]);
-			if(rkeys[i]==currency){
-				r.currency=rkeys[i];
-				r.currency_rate=inusd;
-			}
+		if(!skipPayout){
+			r.lastPayout=r2.data[0];
+			r.addr=addr;
+		} else {
+			r.lastPayout=lr.lastPayout;
+			r.addr=lr.addr;
 		}
-		tmp.sort();
-		r.rates={};
-		for(let i=0;i<tmp.length;i++) r.rates[tmp[i][0]]=tmp[i][1];
-		if(!r.currency || currency=='USD') r.currency='USD';
+		if(!skipRates){
+			var tmp=[];
+			// convert EUR rates to USD rates
+			r3.rates.EUR=1;
+			for(var c in r3.rates){
+				var inusd=new BigNumber(r3.rates[c]).div(r3.rates.USD);
+				tmp.push([c,inusd]);
+				if(lr && c==lr.currency) r.currency=c;
+			}
+			tmp.sort();
+			r.rates={};
+			for(let i=0;i<tmp.length;i++) r.rates[tmp[i][0]]=tmp[i][1];
+			r.ratesTime=nows;
+		} else {
+			r.currency=lr.currency;
+			r.ratesTime=lr.ratesTime;
+			r.rates=lr.rates;
+		}
+		if(!r.currency) r.currency='USD';
+		r.data.ip=r4.ip;
 		if(!r.data.unpaid) r.data.unpaid=0;
 		if(debug) console.log('r=',r);
 		localStorage.setItem('lastupdate',now);
 		localStorage.setItem('lastresponse',JSON.stringify(r));
 		dataDisplay(r);
 	});
-	x0.send(); x1.send(); x2.send(); x3.send(); x4.send();
+	x0.send();
+	x1.send();
+	if(!skipPayout) x2.send();
+	if(!skipRates) x3.send();
+	x4.send();
 }
 
 document.addEventListener('DOMContentLoaded',()=>{
@@ -115,7 +132,7 @@ document.addEventListener('DOMContentLoaded',()=>{
 			addr=obj.data.addr;
 			document.body.innerHTML+='<div id="addr_wrap">'+addr.substr(2)+' <a id="rw" href="javascript:;" title="Remove address">x</a></div><div id="data_wrap"></div>';
 			updateData();
-			setInterval(updateData,35000);
+			setInterval(updateData,30000);
 			document.getElementById('rw').addEventListener('click',function(){
 				if(confirm('Are you sure you want to remove the address?')){
 					chrome.storage.sync.set({data:{addr:''}});
